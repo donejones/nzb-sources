82786ee2885b1cbc0f0c56b4f58fb20edb1de0f0
donejones <123703340+donejones@users.noreply.github.com>
2026-01-02 22:33:37 +0100

    Create forums.ts

---
+++ src/lib/data/forums.ts
@@ -0,0 +1,165 @@
+import type { Forum } from './forumStructure';
+
+export const Forums: Forum[] = [
+  {
+    id: 'abook-link',
+    name: 'ABook.link',
+    registration: 'invite only',
+    content: 'Audiobooks',
+    opened: ''
+  },
+  {
+    id: 'Baka Board',
+    name: 'Baka-Bewerbung.su',
+    registration: 'application',
+    content: 'Animes Ger Sub and Dub',
+    opened: ''
+  },
+  {
+    id: 'Binnews France',
+    name: 'Binnews.eu',
+    registration: 'open',
+    content: 'French',
+    opened: ''
+  },
+  {
+    id: 'Cinema-of-Usenet',
+    name: 'Cinema-of-Usenet.com',
+    registration: 'closed',
+    content: 'German Movie and TV Encodes',
+    opened: '2025-12-24'
+  },
+  {
+    id: 'FileLeechers',
+    name: 'FileLeechers.com',
+    registration: 'closed',
+    content: 'German',
+    opened: '2025-12-24'
+  },
+  {
+    id: 'FileSharingTalk',
+    name: 'FileSharingTalk.com/nzbs/',
+    registration: 'open',
+    content: 'English',
+    opened: ''
+  },
+  {
+    id: 'HDUse',
+    name: 'HDUse.net',
+    registration: 'invite only',
+    content: 'Turkish',
+    opened: ''
+  },
+  {
+    id: 'House-of-Usenet',
+    name: 'House-of-Usenet.com',
+    registration: 'closed',
+    content: 'German',
+    opened: '2025-12-24'
+  },
+  {
+    id: '',
+    name: '',
+    registration: 'closed',
+    content: 'German Usenet forum',
+    opened: ''
+  },
+  {
+    id: 'Kleverig',
+    name: 'Kleverig.eu',
+    registration: 'closed',
+    content: 'XXX',
+    opened: ''
+  },
+  {
+    id: 'NZBCave',
+    name: 'www.NZBCave.co.uk/v3/index.php',
+    registration: 'open',
+    content: 'English',
+    opened: ''
+  },
+  {
+    id: 'NZBforYou',
+    name: 'NZBforYou.com',
+    registration: 'open',
+    content: 'English',
+    opened: ''
+  },
+  {
+    id: 'NZBgrabit',
+    name: 'NZBgrabit.xyz',
+    registration: 'open',
+    content: 'English',
+    opened: ''
+  },
+  {
+    id: 'NZBNewzFrance',
+    name: 'NZBNewzFrance.ninja',
+    registration: 'open',
+    content: 'French',
+    opened: ''
+  },
+  {
+    id: 'SecretBinaries',
+    name: 'SecretBinaries.net',
+    registration: 'closed',
+    content: 'German',
+    opened: ''
+  },
+  {
+    id: '',
+    name: '',
+    registration: 'open',
+    content: 'German Usenet forum',
+    opened: ''
+  },
+  {
+    id: 'Sky-of-Usenet',
+    name: 'Sky-of-Use.net',
+    registration: 'invite only',
+    content: 'German',
+    opened: ''
+  },
+  {
+    id: 'The-Hive',
+    name: 'The-Hive.be/forum',
+    registration: 'open',
+    content: 'English',
+    opened: ''
+  },
+  {
+    id: 'UHD100',
+    name: 'UHD100.com',
+    registration: 'closed',
+    content: 'English full discs',
+    opened: ''
+  },
+  {
+    id: 'Usenet-4All',
+    name: 'Usenet-4All.pw',
+    registration: 'closed',
+    content: 'German',
+    opened: 'never'
+  },
+  {
+    id: 'Usenet-Crew',
+    name: 'Usenet-Crew.com',
+    registration: 'closed',
+    content: 'German',
+    opened: ''
+  },
+  {
+    id: 'UsenetHD',
+    name: 'UsenetHD.li',
+    registration: 'closed',
+    content: 'Spanish',
+    opened: ''
+  },
+  {
+    id: 'VietNZB',
+    name: 'VietNZB.club',
+    registration: 'open',
+    content: 'Vietnamese',
+    opened: ''
+  }
+];
